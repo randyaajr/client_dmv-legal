409aa0694410b37d3732f5f7859930b9304320e0
randyaajr <67880000+randyaajr@users.noreply.github.com>
2021-10-05 23:05:09 -0400

    Created a button which takes user to top of page

--- js/scripts.js
+++ js/scripts.js
@@ -17,3 +17,25 @@ AOS.init({
     return window.innerWidth < maxWidth;
   },
 });
+
+//Get the button
+var mybutton = document.getElementById("toTopBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
